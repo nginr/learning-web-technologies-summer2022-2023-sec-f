36f58c4d92625b64c3b4cfac8e87a7d3b3082a54
nginr <zarifhossainafif@gmail.com>
2023-07-19 01:46:04 +0600

    Wrap every view in a div.

--- Mid-Lab-Exam/views/signin.view.php
+++ Mid-Lab-Exam/views/signin.view.php
@@ -1,22 +1,24 @@
-<fieldset>
-    <legend><b> LOGIN </b></legend>
-    <form action="../controllers/login.controller.php" method="post">
-        <table>
-            <tr>
-                <td>User Id</td>
-                <td>
-                    : <input type="text" placeholder="Enter Id" name="userid" id="userid" required>
-                </td>
-            </tr>
-            <tr>
-                <td> Password </td>
-                <td>
-                    : <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
-                </td>
-            </tr>
-        </table>
-        <hr>
-        <input type="submit" name="submit" value="Sign In">
-        <a href="signup.view.php">Register</a>
-    </form>
-</fieldset>
+<div class="centerdiv">
+    <fieldset>
+        <legend><b> LOGIN </b></legend>
+        <form action="../controllers/login.controller.php" method="post">
+            <table>
+                <tr>
+                    <td>User Id</td>
+                    <td>
+                        : <input type="text" placeholder="Enter Id" name="userid" id="userid" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td> Password </td>
+                    <td>
+                        : <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+                    </td>
+                </tr>
+            </table>
+            <hr>
+            <input type="submit" name="submit" value="Sign In">
+            <a href="signup.view.php">Register</a>
+        </form>
+    </fieldset>
+</div>
--- Mid-Lab-Exam/views/signup.view.php
+++ Mid-Lab-Exam/views/signup.view.php
@@ -1,33 +1,35 @@
-<fieldset>
-    <legend><b> REGISTRATION </b></legend>
-    <form action="../controllers/regristration.controller.php" method="post">
-        <table>
-            <tr>
-                <td>Id</td>
-                <td> : <input type="text" placeholder="Enter Id" name="userid" required> </td>
-            </tr>
-            <tr>
-                <td> Password </td>
-                <td> : <input type="password" placeholder="Enter Password" name="psw" required> </td>
-            </tr>
-            <tr>
-                <td> Confirm Password </td>
-                <td> : <input type="password" placeholder="Repeat Password" name="pswrepeat" required> </td>
-            </tr>
-            <tr>
-                <td>Name</td>
-                <td> : <input type="text" placeholder="Enter Name" name="name" required> </td>
-            </tr>
-            <tr>
-                <td>User Type</td>
-                <td> :
-                    <input type="radio" name="usertype" value="user" checked> <label for="user">User</label>
-                    <input type="radio" name="usertype" value="admin"> <label for="admin">Admin</label>
-                </td>
-            </tr>
-        </table>
-    <hr>
-        <input type="submit" name="submit" value="Sign Up">
-        <a href="signin.view.php">Sign In</a>
-    </form>
-</fieldset>
+<div class="centerdiv">
+    <fieldset>
+        <legend><b> REGISTRATION </b></legend>
+        <form action="../controllers/regristration.controller.php" method="post">
+            <table>
+                <tr>
+                    <td>Id</td>
+                    <td> : <input type="text" placeholder="Enter Id" name="userid" required> </td>
+                </tr>
+                <tr>
+                    <td> Password </td>
+                    <td> : <input type="password" placeholder="Enter Password" name="psw" required> </td>
+                </tr>
+                <tr>
+                    <td> Confirm Password </td>
+                    <td> : <input type="password" placeholder="Repeat Password" name="pswrepeat" required> </td>
+                </tr>
+                <tr>
+                    <td>Name</td>
+                    <td> : <input type="text" placeholder="Enter Name" name="name" required> </td>
+                </tr>
+                <tr>
+                    <td>User Type</td>
+                    <td> :
+                        <input type="radio" name="usertype" value="user" checked> <label for="user">User</label>
+                        <input type="radio" name="usertype" value="admin"> <label for="admin">Admin</label>
+                    </td>
+                </tr>
+            </table>
+            <hr>
+            <input type="submit" name="submit" value="Sign Up">
+            <a href="signin.view.php">Sign In</a>
+        </form>
+    </fieldset>
+</div>
